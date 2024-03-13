d215329e74aadb63817d4760704f16447a03381d
Nestor Lopez <n3zt0r77@gmail.com>
2024-03-13 11:33:14 -0600

    Pages, routing and header created

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout
+        title="Inicio"
+        description="Blog de música, venta de guitarras y más"
+      >
+        <h1>Hola mundo en Next</h1>
+      </Layout>
+    </>
+  );
+}
---
+++ pages/tienda.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout";
+
+export default function Tienda() {
+  return (
+    <Layout
+      title="Tienda Virtual"
+      description="Tienda virtual, venta de guitarras, instrumentos, GuitarLA"
+    >
+      <h1>Tienda Virtual</h1>
+    </Layout>
+  );
+}
